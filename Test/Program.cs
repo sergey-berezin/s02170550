e41db003ae7da043571996bdf82fa87f802690c1
vadim_trib <vdtribrat@yandex.ru>
2020-10-04 14:21:39 +0300

    Works

---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using image_recognizer;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classificator cl = new Classificator(@"C:\Users\vdtri\Pictures\Image_for_c#");
+            cl.recognize();
+        }
+    }
+}
